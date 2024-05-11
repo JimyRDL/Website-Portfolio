7fe007e2d506842bde50fbef837189132121e962
Jaime Rojas <jrojasdl07@gmail.com>
2024-05-11 22:50:11 +0200

    Before navbar interactive

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#dynamic-txt",{
+    strings: ["Game", "Web"],
+    typeSpeed: 250,
+    backSpeed: 200,
+    loop: true,
+    backDelay: 1000,
+  })
+
+  
---
+++ xd.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".dynamic-txt",{
+  strings: ["Game", "Web"],
+  typeSpeed: 250,
+  backSpeed: 200,
+  loop: true,
+  backDelay: 1000,
+})
