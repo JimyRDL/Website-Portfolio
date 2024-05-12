c02afe9a923511d4ea7abed6876f88c8a031f8d6
Jaime Rojas <jrojasdl07@gmail.com>
2024-05-12 20:00:55 +0200

    Navbar added, previous to next section

--- navbartry.js
+++ navbartry.js
@@ -1,18 +1,7 @@
-var typed = new Typed("#dynamic-txt",{
-    strings: ["Game", "Web"],
-    typeSpeed: 250,
-    backSpeed: 200,
-    loop: true,
-    backDelay: 1000,
-  })
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('.links-buttons');
-
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 350;
+    let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
@@ -21,7 +10,6 @@ window.onscroll = () => {
         link.classList.remove('active');
       });
       
-      // Find the corresponding navigation link based on the section's ID
       let correspondingNavLink = document.querySelector(`#navbar a[href="#${id}"]`);
       if (correspondingNavLink) {
         correspondingNavLink.classList.add('active');
