5ffcdb26e53da88e684d7a0543c491c81480c20b
Jaime Rojas <jrojasdl07@gmail.com>
2024-06-14 21:36:20 +0200

    No carousel

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let navLinks = document.querySelectorAll('.links-buttons');
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 400;
+    let offset = sec.offsetTop - 540;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
 
@@ -57,81 +57,3 @@ function showDiv(sectionId, divId, buttonId) {
     targetButton.classList.add('activeButton');
   }
 }
-
-(function() {
-  "use strict";
-
-  var carousel = document.getElementsByClassName('carousel')[0],
-      slider = carousel.getElementsByClassName('carousel__slider')[0],
-      items = carousel.getElementsByClassName('carousel__slider__item'),
-      prevBtn = carousel.getElementsByClassName('carousel__prev')[0],
-      nextBtn = carousel.getElementsByClassName('carousel__next')[0];
-  
-  var width, height, totalWidth, margin = 20,
-      currIndex = 0,
-      interval, intervalTime = 4000;
-  
-  function init() {
-      resize();
-      move(Math.floor(items.length / 2));
-      bindEvents();
-  }
-  
-  function resize() {
-      width = Math.max(window.innerWidth * .3, 275),
-      height = window.innerHeight * .3,
-      totalWidth = width * items.length;
-    
-      slider.style.width = totalWidth + "px";
-    
-      for(var i = 0; i < items.length; i++) {
-          let item = items[i];
-          item.style.width = (width - (margin * 2)) + "px";
-          item.style.height = height + "px";
-      }
-  }
-  
-  function move(index) {
-    
-      if(index < 1) index = items.length;
-      if(index > items.length) index = 1;
-      currIndex = index;
-    
-      for(var i = 0; i < items.length; i++) {
-          let item = items[i],
-              box = item.getElementsByClassName('item__3d-frame')[0];
-          if(i == (index - 1)) {
-              item.classList.add('carousel__slider__item--active');
-              box.style.transform = "perspective(1200px)"; 
-          } else {
-            item.classList.remove('carousel__slider__item--active');
-              box.style.transform = "perspective(1200px) rotateY(" + (i < (index - 1) ? 40 : -40) + "deg)";
-          }
-      }
-    
-      slider.style.transform = "translate3d(" + ((index * -width) + (width / 2) + window.innerWidth / 2) + "px, 0, 0)";
-  }
-  
-  
-  function prev() {
-    move(--currIndex);
-  }
-  
-  function next() {
-    move(++currIndex);    
-  }
-  
-  
-  function bindEvents() {
-      window.onresize = resize;
-      prevBtn.addEventListener('click', () => { prev(); });
-      nextBtn.addEventListener('click', () => { next(); });    
-  }
-
-
-
-
-  
-  init();
-  
-})();
